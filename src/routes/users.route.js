6768211eb4e7269344a5de90a345949d6be95080
andreadityayanwar <andreadityayanwar@gmail.com>
2022-10-19 17:09:42 +0700

    require validator.middleware

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,10 +2,12 @@ const users = require ("express").Router();
 
 const userController = require("../controllers/users.controller");
 
-users.post("/", userController.createUser);
-users.get("/", userController.readAllUsers);
-users.get("/:id", userController.readUserById);
-users.put("/:id", userController.editUserById);
+const {check, paramUUID, basicUserCreds, paging} = require("../middlewares/validator.middleware");
+
+users.post("/", basicUserCreds, check, userController.createUser);
+users.get("/", paging, check, userController.readAllUsers);
+users.get("/:id", paramUUID, check, userController.readUserById);
+users.put("/:id", paramUUID, basicUserCreds, check, userController.editUserById);
 users.delete("/:id", userController.deleteUserById);
 
 module.exports = users;
