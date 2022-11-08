f0de2aba3b684c3ee026485d9b40070c1d0624eb
andreadityayanwar <andreadityayanwar@gmail.com>
2022-11-08 13:52:05 +0700

    supabash connection

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg");
 
 const db = new Pool({
-  connectionString : "postgresql://postgres:admin@localhost:5432/postgres?schema=public"
+  connectionString : "postgresql://postgres:AndreAditya020101.@db.ungquuggeexomajrklzd.supabase.co:5432/postgres?schema=public"
 });
 
 module.exports = db;
